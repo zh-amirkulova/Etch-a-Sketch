defbe41d7e2118dfc8bce000a8906fc3013e6ee9
Zhanerke <zhanerke.amirkulova@gmail.com>
2025-02-08 22:06:20 +0500

    Создала сетку 16x16

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const divMain = document.createElement('div');
+const divConteiner = document.querySelector('div');
+
+for (let i=0; i<256; i++){
+    const div = document.createElement('div');
+    divConteiner.append(div);
+    div.style.cssText = `
+        width: 46px;
+        height: 46px;
+        border: 1px solid rgba(137, 81, 41, 0.2);; 
+        
+    `;
+    divConteiner.innerHTML.div;
+    console.log(div);
+};
+
+
