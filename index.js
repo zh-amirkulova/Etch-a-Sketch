fce2a78474448372d768947ec55fa3575563867d
Zhanerke <zhanerke.amirkulova@gmail.com>
2025-02-09 09:38:43 +0500

    Добавлен эффект hover для изменения цвета при наведении

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ for (let i=0; i<256; i++){
     const div = document.createElement('div');
     divConteiner.append(div);
     div.style.cssText = `
-        width: 46px;
-        height: 46px;
-        border: 1px solid rgba(137, 81, 41, 0.2);; 
-        
+        width: 23px;
+        height: 23px;
+        border: 1px solid rgba(137, 81, 41, 0.05);
     `;
     divConteiner.innerHTML.div;
-    console.log(div);
+    
+    const hover = document.querySelector('div');
+    console.log(hover);
+
+    hover.addEventListener('mouseover', function(e){
+    e.target.style.backgroundColor = 'pink';
+});
 };
 
 
+
+ 
